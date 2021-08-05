6bc1a1a188206171720ba76ae22d7a8314056fc2
radhoo <radhoo.tech@gmail.com>
2021-08-05 20:41:43 +0300

    versioning + cc licensing

--- code/smoggie/src/config/devices-17/17000XXX_MHZ19B.h
+++ code/smoggie/src/config/devices-17/17000XXX_MHZ19B.h
@@ -1,18 +1,25 @@
 /**
- *	License:	Licensed under CC BY NC 4.0
- *  			This license lets others remix, adapt, and build upon your work non-commercially, and although their new works must also acknowledge you 
- *  			and be non-commercial, they don’t have to license their derivative works on the same terms.
- *  			You should have received a copy of the license with this work.
  *
- *  Project:    SMOGGIE is an ultra-low cost automated air quality monitor with a rain proof enclosure and a simple mount system to make installation easy. 
- *  			It features a high quality laser scatering Particulate Matter sensor for PM1, PM2.5 and PM10 and an additional sensor for temperature, pressure and humidity. 
- *  			It connects to the internet via Wifi and can be powered by a standard 5V micro-usb cable. Readings are accessed via the uRADMonitor API or decentralized via your local network. This monitor is lab tested for data accuracy.
- *
- *  Author:  	Magnasci SRL , www.magnasci.com 
- *	Web:		www.uradmonitor.com
+ *  License:  GPL v3
+ *  Project:  SMOGGIE is an ultra-low cost automated air quality monitor with a rain proof enclosure and a simple mount system to make installation easy. 
+ *            It features a high quality laser scatering Particulate Matter sensor for PM1, PM2.5 and PM10 and an additional sensor for temperature, pressure and humidity. 
+ *            It connects to the internet via Wifi and can be powered by a standard 5V micro-usb cable. Readings are accessed via the uRADMonitor API or decentralized via your local network. This monitor is lab tested for data accuracy.
  *
  *  Copyright 2013-2015 Radu Motisan, radu.motisan@gmail.com
- *	Copyright 2015-2021 Magnasci SRL, www.magnasci.com
+ *  Copyright 2015-2021 Magnasci SRL, www.magnasci.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 #pragma once
